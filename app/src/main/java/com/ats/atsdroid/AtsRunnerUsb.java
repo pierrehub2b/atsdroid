1b2360039b416fb66d221dce2161def6b23c7f9e
Anthony D'HIERRE <anthony.dhierre@icloud.com>
2020-03-13 09:07:22 +0100

    Stop WebSocket server when driver is stopped

--- app/src/main/java/com/ats/atsdroid/AtsRunnerUsb.java
+++ app/src/main/java/com/ats/atsdroid/AtsRunnerUsb.java
@@ -1,15 +1,32 @@
 package com.ats.atsdroid;
 
 import com.ats.atsdroid.server.AtsWebSocketServer;
+
+import java.io.IOException;
 import java.net.InetSocketAddress;
 
 public class AtsRunnerUsb extends AtsRunner {
+
+    private AtsWebSocketServer server;
+
+    @Override
+    public void stop() {
+        try {
+            server.stop();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        super.stop();
+    }
 
     @Override
     public void testMain() {
         super.testMain();
 
-        AtsWebSocketServer server = new AtsWebSocketServer(new InetSocketAddress(port), automation);
+        server = new AtsWebSocketServer(new InetSocketAddress(port), automation);
         Thread thread = new Thread(server);
         thread.start();
 
