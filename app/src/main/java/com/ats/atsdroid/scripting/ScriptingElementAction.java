50519aa58a25501c6565fc0e4bb94d2695323231
pierrehub2b <pierrehuber@yahoo.fr>
2020-04-22 08:11:01 +0200

    Update ScriptingElementAction.java

--- app/src/main/java/com/ats/atsdroid/scripting/ScriptingElementAction.java
+++ app/src/main/java/com/ats/atsdroid/scripting/ScriptingElementAction.java
@@ -30,4 +30,35 @@ public class ScriptingElementAction extends ScriptingAction {
                 throw new Exception("bad scripting action");
         }
     }
+
+    public void longPress(String value){
+        // if value is not integer throw code exception
+    }
+
+    public void tap(String value){
+        // if value is not integer throw code exception
+    }
+
+    public void setAirPlaneMode(String value){
+        // if value is not boolean throw code exception
+        // if usb mode 'on' throw driver exception
+    }
+
+    public void setWifiEnabled(String value){
+        // if value is not boolean throw code exception
+        // if usb mode 'on' throw driver exception
+    }
+
+    public void setBluetoothEnabled(String value){
+        // if value is not boolean throw code exception
+    }
+
+    public void setOrientation(String value){
+        // if value not equals 'portrait' or 'landscape' throw code exception
+    }
+
+    public String getBluetoothName(){
+        return "";
+    }
+
 }
