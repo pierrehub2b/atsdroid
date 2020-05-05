e2e27404311040077412c9e35e49e4d84ff5456e
Anthony D'HIERRE <anthony.dhierre@agilitest.com>
2020-05-05 17:24:42 +0200

    Edit error description

--- app/src/main/java/com/ats/atsdroid/exceptions/DriverException.java
+++ app/src/main/java/com/ats/atsdroid/exceptions/DriverException.java
@@ -2,8 +2,8 @@ package com.ats.atsdroid.exceptions;
 
 public class DriverException extends Exception {
 
-    public static final String UNAVAILABLE_FEATURE = "Feature is unavailable";
-    public static final String UNKNOWN_FUNCTION = "Feature is unavailable";
+    public static final String UNAVAILABLE_FEATURE = "Unavailable command";
+    public static final String UNKNOWN_FUNCTION = "Unknown command";
     public static final String UNKNOWN_ERROR = "Driver error";
 
      public DriverException(String message) {
